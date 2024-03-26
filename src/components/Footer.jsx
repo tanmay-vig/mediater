659b8b04465f754c346cf0f06072ec74719526d7
Tanmay <cooltanmayvig@gmail.com>
2024-03-27 00:54:55 +0530

    new-changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Logo from "../assets/mediater_logo.jpg";
 
 export default function Footer() {
     return (
@@ -9,10 +10,13 @@ export default function Footer() {
                 variant="dark"
                 data-bs-theme="dark"
             >
-                <Row>
+                <Row xs={1} md={4}>
+                    <Col>
+                        <img src={Logo} alt="logo" />
+                    </Col>
                     <Col>
                         <h2>Mediater</h2>
-                        <div className="">
+                        <div>
                             Mediater is a platform that connects brands with
                             influencers and creators. We help brands to find the
                             right influencers and creators to promote their
@@ -107,16 +111,15 @@ export default function Footer() {
                                         viewBox="0 0 16 16"
                                     >
                                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-                                    </svg> official@mediater.in{" "}
+                                    </svg>{" "}
+                                    official@mediater.in{" "}
                                 </a>
                             </Row>
-                            <Row>
-                                Phone: +917994076261
-                            </Row>
+                            <Row>Phone: +917994076261</Row>
                             <Row>Address: Karicode, Kollam</Row>
                         </Container>
                     </Col>
-                </Row> 
+                </Row>
             </Container>
         </section>
     );
